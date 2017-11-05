0d4155f239da6dc0d9da6a96232933d5b22115b2
ayubou <volg108allo@gmail.com>
2017-11-05 21:20:34 +0900

    オリジナルアプリ

---
+++ Assets/EnemiesMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesMove : MonoBehaviour {
+
+	// 落下速度
+	private float speed = -0.01f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, this.speed, 0);
+		if (transform.position.y < -5.5f) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+
+	public GameObject[] Train;
+
+
+	// Use this for initialization
+	void Start () {
+		CreateEnemie ();
+		//Vector2 tmp = Train[0].transform.position;
+		//var sh = Train[0].GetComponent<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (transform.position.y > 4.0f) {
+		//CreateEnemie ();
+		//}
+		var test = transform.position.y;
+		Debug.Log (test);
+	}
+
+
+
+
+	void CreateEnemie(){
+
+		//初期の配置座標を設定
+		Vector2 placePosition = new Vector2(-2.05f,4.0f);
+
+		//回転角度の設定
+		Quaternion q = new Quaternion();
+		q= Quaternion.identity;
+
+		//配置
+		for (int i = 0; i < 5; i++){
+
+			//ランダム表示
+			int rand = Random.Range (0, Train.Length);
+			Instantiate(Train[rand], placePosition, q);
+
+			//横幅の取得
+			var sr = Train[rand].GetComponent<SpriteRenderer>();
+			placePosition.x += sr.bounds.size.x + 0.15f;
+
+		}
+	}
+}
