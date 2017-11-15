8b2ce6d53f114acd31d933e4eab44f46a60aab61
ayubou <volg108allo@gmail.com>
2017-11-15 21:53:29 +0900

    Raycastを実装したいが動かない

---
+++ Assets/KurekureTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KurekureTouch : TapBehaviour {
+
+	public override void TapDown (ref RaycastHit hit){
+		Debug.Log("タップしたときの処理");
+
+	}
+
+	public override void TapUp (ref RaycastHit hit){
+		Debug.Log("タップを離したときの処理");
+
+	}
+
+	void Update(){
+		// タップされてるかをフラグで管理したりして適当に更新する。
+	}
+}
---
+++ Assets/TapBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapBehaviour : MonoBehaviour {
+
+	// タッチしたときに呼ばれる。
+	public virtual void TapDown(ref RaycastHit hit){}
+	// タッチを離したときに呼ばれる。
+	public virtual void TapUp(ref RaycastHit hit){}
+}
